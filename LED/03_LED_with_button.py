374ba29e5c09d758e6fcac87620c98dc724d5b56
Gregory Lazaridis <gregory.lazaridis@westernacher.com>
2018-06-24 14:25:05 +0200

    finish 03

--- LED/03_LED_with_button.py
+++ LED/03_LED_with_button.py
@@ -4,24 +4,22 @@ import time
 
 LedPin = 11 # pin11
 ButtonPin = 13
-Warten = 0.04
 
 def setup():
   GPIO.setmode(GPIO.BOARD)
   GPIO.setup(ButtonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-  #GPIO.setup(LedPin, GPIO.OUT)
-  #GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
+  GPIO.setup(LedPin, GPIO.OUT)
+  GPIO.output(LedPin, GPIO.LOW)
 
 def loop():
-  while True:
-    print '...led on'
-    time.sleep(Warten)
-    GPIO.output(LedPin, GPIO.LOW) # led on time.sleep(0.5)
-    time.sleep(Warten)
-    print 'led off...'
-    print GPIO.input(ButtonPin)
-    GPIO.output(LedPin, GPIO.HIGH) # led off time.sleep(0.5)
-    #time.sleep(1)
+    while True:
+      if GPIO.input(ButtonPin) == 1:
+          print '...led on'
+          GPIO.output(LedPin, GPIO.HIGH) # turn led on
+          time.sleep(0.05)
+          if GPIO.input(ButtonPin) == 0:
+            print '...led off'
+            GPIO.output(LedPin, GPIO.LOW) # rutn led on
 
 def destroy():
   GPIO.output(LedPin, GPIO.HIGH) # led off
@@ -33,4 +31,3 @@ if __name__ == '__main__':
     loop()
   except KeyboardInterrupt:
     destroy()
-
