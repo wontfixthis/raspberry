810f9d5b41603f7c478acc8869a8dddf59a5f82e
Gregory Lazaridis <gregory.lazaridis@westernacher.com>
2018-06-24 13:15:45 +0200

    add led scripts

---
+++ LED/02_LED_on_off.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+LedPin = 11 # pin11
+Warten = 0.04
+
+def setup():
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(LedPin, GPIO.OUT)
+  GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
+
+def loop():
+  while True:
+    print '...led on'
+    time.sleep(Warten)
+    GPIO.output(LedPin, GPIO.LOW) # led on time.sleep(0.5)
+    time.sleep(Warten)
+    print 'led off...'
+    GPIO.output(LedPin, GPIO.HIGH) # led off time.sleep(0.5)
+    #time.sleep(1)
+
+def destroy():
+  GPIO.output(LedPin, GPIO.HIGH) # led off
+  GPIO.cleanup()
+
+if __name__ == '__main__':
+  setup()
+  try:
+    loop()
+  except KeyboardInterrupt:
+    destroy()
+
